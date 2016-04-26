c1a1cb7460bb5d281a2a9d18592a9b6b628d9a0f
Hyeseong Kim <hyeseong.kim@architectgroup.com>
2016-04-26 17:28:04 +0900

    update global script
    Co-authored-by: Hyeseong Kim <cometkim.kr@gmail.com>

--- xbeamerchart/js/all.js
+++ xbeamerchart/js/all.js
@@ -1,35 +1,52 @@
-function replaceAll(str, searchStr, replaceStr) {
-    return str.split(searchStr).join(replaceStr);
-}
+(function($){
+    var DEFAULT_TRUNCATE_CHARS = 60;
 
-function truncateText(text, numberOfChars) {
-    numberOfChars = numberOfChars || DEFAULT_TRUNCATE_CHARS;
-    if (text.length > numberOfChars) {
-        return (text.substring(0, numberOfChars) + "...");
+    String.prototype.includes = String.prototype.includes || function(chars){
+        return this.indexOf(chars) != -1;
+    };
+
+    function replaceAll(str, searchStr, replaceStr) {
+        return str.split(searchStr).join(replaceStr);
     }
-    return text;
-}
 
-$(function(){
-    $('.XBeamerWidget-select').each(function(){
-        $(this).multiselect({multiple: false});
-        if($(this).hasClass('with-filter')){
-            $(this).multiselectfilter();
+    function truncateText(text, numberOfChars) {
+        numberOfChars = numberOfChars || DEFAULT_TRUNCATE_CHARS;
+        if (text.length > numberOfChars) {
+            return (text.substring(0, numberOfChars) + "...");
         }
+        return text;
+    }
+
+    $(function(){
+        $('.XBeamerWidget select').each(function(){
+            $(this).multiselect({
+                classes: 'queryConditionSelector',
+                multiple: $(this).is('[multiple]'),
+                selectedText: function(numChecked, numTotal, checkedItems) {
+                    var value = [];
+                    var $checkedItems = $(checkedItems);
+
+                    var target = $checkedItems.first().attr('name').split('_')[1];
+                    var $target = $('#' + target).closest('.select');
+
+                    console.log(target);
+                    $checkedItems.each(function(){
+                        var valueString = $(this).next().html();
+                        if ( $target.hasClass('user') && valueString.includes(' (')){
+                            valueString = valueString.substring(0, valueString.indexOf(' ('));
+                        }
+                        value.push(valueString);
+                    });
+                    var joinedText = value.join(", ");
+                    return truncateText(joinedText);
+                }
+            });
+            if($(this).hasClass('with-filter')){
+                $(this).multiselectfilter();
+            }
+        });
+
+        $('.XBeamerWidget > .help').tooltip();
     });
 
-    $('.XBeamerWidget-multiselect').each(function(){
-        $(this).multiselect({multiple: true});
-        if($(this).hasClass('with-filter')){
-            $(this).multiselectfilter();
-        }
-    });
-
-    $('.XBeamerWidget > .help').tooltip();
-
-    $('.ui-icon-minusthick').click(function() {
-        var icon = $( this );
-        icon.toggleClass('ui-icon-minusthick ui-icon-plusthick');
-        icon.parent().parent().find('.content').toggle();
-    });
-});
+})(jQuery);
