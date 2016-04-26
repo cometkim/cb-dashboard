084125f63f6961ca195ba924780d71aa8005a82d
Hyeseong Kim <hyeseong.kim@architectgroup.com>
2016-04-26 11:30:15 +0900

    prepare a basic of widget, can be used for simple widgets
    Co-authored-by: Hyeseong Kim <cometkim.kr@gmail.com>

---
+++ src/com/architectgroup/xbeamerchart/widget/base/XBeamerBasicWidget.java
@@ -0,0 +1,40 @@
+package com.architectgroup.xbeamerchart.widget.base;
+
+import com.ecyrd.jspwiki.WikiContext;
+import org.apache.velocity.VelocityContext;
+
+import javax.annotation.Nullable;
+import javax.validation.constraints.NotNull;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by comet on 2016-04-26.
+ */
+public class XBeamerBasicWidget extends XBeamerWidget {
+    private String htmlTag;
+    private Map<String, String> attributes;
+
+    public XBeamerBasicWidget(WikiContext ctx, String htmlTag) {
+        super(ctx, "xbeamerchart/includes/widget-basic.vm");
+        this.htmlTag = htmlTag;
+        this.attributes = new HashMap<>();
+    }
+
+    public Map<String, String> getAttributes(){ return this.attributes; }
+    public String attribute(String attr){ return this.attributes.get(attr); }
+    public void attribute(String attr, String value){ this.attributes.put(attr, value); }
+
+    public String getHtmlTag(){ return this.htmlTag; }
+    public void setHtmlTag(String htmlTag){ this.htmlTag = htmlTag; }
+
+    @Override
+    public void populateContext(VelocityContext velocityContext) {
+        velocityContext.put("htmlTag", this.htmlTag);
+        velocityContext.put("attributes", this.attributes);
+    }
+
+    @Nullable
+    @Override
+    public String getSubject(@NotNull String argument) { return argument; }
+}
