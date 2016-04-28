a1fa085ab926446ca690558250487f7402fb70b3
Hyeseong Kim <hyeseong.kim@architectgroup.com>
2016-04-28 10:27:28 +0900

    fix a bug on select options
    Co-authored-by: Hyeseong Kim <cometkim.kr@gmail.com>

--- src/com/intland/codebeamer/wiki/plugins/XBeamerIssueCountByFiledPlugin.java
+++ src/com/intland/codebeamer/wiki/plugins/XBeamerIssueCountByFiledPlugin.java
@@ -56,17 +56,17 @@ public class XBeamerIssueCountByFiledPlugin extends XBeamerWrapperPlugin {
         this.addWidgetForParameter("display", displayWidget);
 
         Map<String, String> supportedFields = new LinkedHashMap<>();
-        displayTypes.put("assignedto", "Assignee");
-        displayTypes.put("category", "Category");
-        displayTypes.put("detected", "Detected");
-        displayTypes.put("os", "OS");
-        displayTypes.put("platform", "Platform");
-        displayTypes.put("priority", "Priority");
-        displayTypes.put("resolution", "Resolution");
-        displayTypes.put("severity", "Severity");
-        displayTypes.put("status", "Status");
-        displayTypes.put("submitter", "Submitter");
-        displayTypes.put("target", "Target");
+        supportedFields.put("assignedto", "Assignee");
+        supportedFields.put("category", "Category");
+        supportedFields.put("detected", "Detected");
+        supportedFields.put("os", "OS");
+        supportedFields.put("platform", "Platform");
+        supportedFields.put("priority", "Priority");
+        supportedFields.put("resolution", "Resolution");
+        supportedFields.put("severity", "Severity");
+        supportedFields.put("status", "Status");
+        supportedFields.put("submitter", "Submitter");
+        supportedFields.put("target", "Target");
 
         fieldWidget = new XBeamerSelectWidget(context, supportedFields, false);
         fieldWidget.setDefaultArgument("status");
